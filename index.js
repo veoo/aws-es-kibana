c8e58c211dd84245332b15fe17cc73ad188f6504
Alastair Brunton <info@simplyexcited.co.uk>
2016-07-20 16:46:40 +0200

    adding basic auth

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var stream = require('stream');
 var figlet = require('figlet');
+var basicAuth = require('basic-auth-connect');
 
 var yargs = require('yargs')
     .usage('usage: $0 [options] <aws-es-cluster-endpoint>')
@@ -91,7 +92,7 @@ var proxy = httpProxy.createProxyServer({
 });
 
 var app = express();
-app.use(express.basicAuth('user', 'secret'));
+app.use(basicAuth('user', 'secret'));
 app.use(bodyParser.raw({type: '*/*'}));
 app.use(getcreds);
 app.use(function (req, res) {
